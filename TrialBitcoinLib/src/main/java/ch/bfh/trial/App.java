2e6322a0c9e3c063632b99828b3f8ecd49083028
Julian <julianstampfli4@gmail.com>
2018-05-02 09:31:15 +0200

    some changes for bitcoin lib

--- TrialBitcoinLib/src/main/java/ch/bfh/trial/App.java
+++ TrialBitcoinLib/src/main/java/ch/bfh/trial/App.java
@@ -3,12 +3,13 @@ import fr.acinq.bitcoin.*;
 import wf.bitcoin.javabitcoindrpcclient.*;
 
 import java.net.URL;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -22,20 +23,37 @@ public class App
             BitcoinJSONRPCClient client = new BitcoinJSONRPCClient(u);
             BitcoindRpcClient.Block block2 = client.getBlock(client.getBlockCount());
             System.out.println(block2.toString());
-            System.out.println(client.getRawTransactionHex("88fa46d5fa8ee676b0b242b7e4033e179b3332d19f4622ab33e7391b4a8933ef"));
+            //System.out.println(client.getRawTransactionHex("c90ff2d1e031af63742cb1ca2267ba40cff80349eb7e271703adb5143b202a12"));
 
             System.out.println(block2.time().toString());
             BitcoindRpcClient.Block block3 = client.getBlock(1);
             System.out.println(block3.toString());
 
-            System.out.println(client.getRawTransactionHex("88fa46d5fa8ee676b0b242b7e4033e179b3332d19f4622ab33e7391b4a8933ef"));
-            BitcoindRpcClient.RawTransaction transaction =client.decodeRawTransaction(client.getRawTransactionHex("88fa46d5fa8ee676b0b242b7e4033e179b3332d19f4622ab33e7391b4a8933ef"));
-            System.out.println(transaction.toString());
             System.out.println(block3.time().toString());
             for(int i=client.getBlockCount()-20;i<=client.getBlockCount();i++)
             {
-                System.out.println( client.getBlock(i).time().toString());
+                BitcoindRpcClient.Block blocki = client.getBlock(i);
+                System.out.println( blocki.time().toString());
+
+                for(BitcoindRpcClient.RawTransaction tx : blocki.tx().stream().map(x->client.decodeRawTransaction(client.getRawTransactionHex(x))).collect(Collectors.toList()))
+                {
+                    System.out.println(tx.txId());
+                    System.out.println(tx.vOut().stream().map(x->x.scriptPubKey().type()).reduce("",String::concat));
+                }
             }
+
+            BitcoindRpcClient.Block block4 = client.getBlock(834624);
+            System.out.println(block4.toString());
+            for(BitcoindRpcClient.RawTransaction tx : block2.tx().stream().map(x->client.decodeRawTransaction(client.getRawTransactionHex(x))).collect(Collectors.toList()))
+            {
+                System.out.println(tx.txId());
+                System.out.println(tx.vOut().stream().map(x->x.scriptPubKey().type()).reduce("",String::concat));
+            }
+
+            BitcoindRpcClient.RawTransaction transaction =client.decodeRawTransaction(client.getRawTransactionHex("8cd3ef3817b5e6a072a9803eafb041140e180936915c00b5aa22785cd6c46f77"));
+            System.out.println(transaction.toString());
+
+
         }
         catch (Exception e){
             System.out.println(e.toString());
